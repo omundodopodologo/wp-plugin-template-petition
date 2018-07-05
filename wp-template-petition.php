6e1ac5bbbcc0ea4e2c15fc083b4e9954d4551452
Walace Silva <wsilva.emp@gmail.com>
2018-07-05 15:09:52 -0300

    Update and rename wp-template-petition.php to wp-plugin-template-petition.php

--- wp-template-petition.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Template Name: WP Template Petition
- * Template Post Type: page
- *
- * @package O Mundo do Podólogo
- * @subpackage Wordpress
- * @since O Mundo do Podólogo - Petition 1.0
- */
