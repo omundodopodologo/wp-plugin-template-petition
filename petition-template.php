33a7b0160450c7c9fea46343a9d4e2bdc0c8c795
Walace Silva <wsilva.emp@gmail.com>
2018-07-05 15:13:03 -0300

    Create petition-template.php

---
+++ petition-template.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template Name: Petition Template
+ * Template Post Type: post, page
+ *
+ * @package O Mundo do Podólogo
+ * @subpackage Wordpress
+ * @since O Mundo do Podólogo - Petition 1.0
+ */
